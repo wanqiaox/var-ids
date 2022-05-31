45492708bf913be760d239ba814e939c3eab790f
Wanqiao <wanqiaox@stanford.edu>
2022-05-31 13:46:29 -0700

    debugged

--- agent/var_ids.py
+++ agent/var_ids.py
@@ -19,21 +19,35 @@ class Agent(metaclass=abc.ABCMeta):
     # Select an action.
     raise NotImplementedError
 
+def random_argmin(rng, scores: np.ndarray):
+  scores = scores.reshape(-1)
+  probs = (scores==scores.min()).astype(np.float32)
+  probs /= probs.sum()
+  return rng.choice(np.arange(scores.size), p=probs)
+
+def random_argmax(rng, scores: np.ndarray):
+  scores = scores.reshape(-1)
+  probs = (scores==scores.max()).astype(np.float32)
+  probs /= probs.sum()
+  return rng.choice(np.arange(scores.size), p=probs)
+
 def value_iter(t, p, r):
+  # if t == 4:
+  #   print("ahh")
   num_action = p.shape[2]
   num_state = p.shape[1]
   num_samples = p.shape[0]
-  H = (num_state + 1) / 2 # TODO: check
+  H = (num_state + 1) // 2 # TODO: check
   V = np.zeros((num_samples, num_state,))
   for _ in range (H - 1, t-1, -1):
-    Q = r + np.sum(p * V, axis=-1)
-    V = np.amax(Q, axis=-1)
+    Q = r + np.sum(p * V[:,None,None,:], axis=-1)
+    V = np.max(Q, axis=-1)
 #     Q = (p*(r + V[:,None]).transpose()[None]).sum(axis=-1) #shape: (state, actionn)
 #     V = Q.max(axis=-1) #(state,)
   return Q, V
 
-def var_ids_action(rng, t, state, num_trans: np.ndarray, num_success: np.ndarray, num_failure: np.ndarray,
-                   num_good_obs: np.ndarray, num_bad_obs: np.ndarray, obs_masks: np.ndarray, num_sample: int = 32):
+def var_ids_pess_action(rng, pess_factor, t, state, num_trans: np.ndarray, num_success: np.ndarray, num_failure: np.ndarray,
+                   num_good_obs: np.ndarray, num_bad_obs: np.ndarray, num_sample: int = 32):
   num_action = num_trans.shape[1]
   num_state = num_trans.shape[0]
 #   num_action = len(num_success)
@@ -41,47 +55,144 @@ def var_ids_action(rng, t, state, num_trans: np.ndarray, num_success: np.ndarray
 #   p_samples = rng.beta(np.tile(num_success + 1, (num_sample, 1)), np.tile(num_failure + 1, (num_sample, 1)))
   trans_shape = num_trans.shape
   reward_shape = num_success.shape
-  p_samples = rng.dirichlet(np.tile(num_trans.reshape(-1), (num_sample, 1)))
+  pp = np.zeros((num_state, num_action, num_state))
+  for i in range(num_state-2):
+    if i%2 ==0:
+      pp[i,0,i+2] = 1
+      pp[i,1,i+1] = 1
+    else:
+      pp[i,0,i+2]=1
+      pp[i,1,i+2]=1
+  # pp[num_state-2,0,num_state-2]=pp[num_state-1,0,num_state-1]=pp[num_state-2,1,num_state-2]=pp[num_state-1,1,num_state-1]=1
+  pp[num_state-2,0,0]=pp[num_state-1,0,0]=pp[num_state-2,1,0]=pp[num_state-1,1,0]=1
+  p_samples = np.tile(pp, (num_sample, 1,1,1))
+  # p_samples = np.stack([np.stack([rng.dirichlet(num_trans[s,a], size=num_sample) for a in range(num_action)], axis=1) for s in range(num_state)], axis=1)
   r_samples = rng.beta(np.tile((num_success + 1).reshape(-1), (num_sample, 1)), np.tile((num_failure + 1).reshape(-1), (num_sample, 1)))
+  r_samples = r_samples.reshape(num_sample, *reward_shape)
+  # r_samples[:, np.arange(0, num_state, 2), 0] = 0
+  # r_samples[:, np.arange(1, num_state, 2), :] = 0
+  # r_samples[:,num_state-1,:] = 0
+   
   c_samples = rng.beta(np.tile((num_good_obs + 1).reshape(-1), (num_sample, 1)), np.tile((num_bad_obs + 1).reshape(-1), (num_sample, 1)))
-  
-  Q_hat, V_hat = value_iter(t, p_samples.reshape(num_sample, *trans_shape), r_samples.reshape(num_samples, *reward_shape))
-  Q_cross, _ = value_iter(t, p_samples.reshape(num_sample, *trans_shape), c_samples.reshape(num_samples, *reward_shape))
+  c_samples = c_samples.reshape(num_sample, *reward_shape)
+  # c_samples[:, np.arange(0, num_state, 2), 0] = 0
+  # c_samples[:, np.arange(1, num_state, 2), :] = 0
+  # c_samples[:,num_state-1,:] = 0
+
+  Q_hat, V_hat = value_iter(t, p_samples.reshape(num_sample, *trans_shape), r_samples)
+  Q_cross, _ = value_iter(t, p_samples.reshape(num_sample, *trans_shape), c_samples)
 
   # Minimize information ratio over 2-sparse policies
-  info_ratio = np.zeros([num_action, num_action])
-  
   A1 = (V_hat[:,state, None] - Q_hat[:,state,:]).mean(0)
   A2 = A1
-  C1 = np.array([np.trace(np.cov(Q_cross[:,state,a])) for a in range(num_action)])
+  C1 = np.array([np.cov(Q_cross[:,state,a]) for a in range(num_action)])
   C2 = C1
-
-  f_alpha = 4*(A1[None,:] - A2[:,None])*(A2[:,None]*C1[None,:] - A1[None,:]*C2[:,None]) / (C1[None,:] - C2[:,None])**2
-  f_0 = np.tile(A2[:,None]**2 / C2[:,None], (1, num_action))
-  f_1 = np.tile(A1[None,:]**2 / C1[None,:], (num_action, 1))
-  alphas = (A2[:,None]*C1[None,:] - 2*A1[None,:]*C2[:,None] + np.tile(A2[:,None]*C2[:,None], (1, num_action))) / (C1[None,:] - C2[:,None])*(A1[None,:] - A2[:,None])
-  f_matrix = np.stack([f_0, f_alpha, f_1], axis=0)
-  index = np.argmax(f_matrix)
-  index = np.unravel_index(index, f_matrix.shape)
-  assert f_matrix[index[0],index[1],index[2]] == f_matrix.max()
-    
-  alpha = alphas[index[1], index[2]] if index[0] == 1 else index[0]
-  action = np.zeros(num_action)
-  action[index[1]] = alpha
-  action[index[2]] = 1 - alpha
   
-  return rng.choice(np.arange(num_action), p=action)
+  alpha_1 = (-np.sqrt((C1[None,:] - C2[:,None])**2*pess_factor + (A1[None,:]*C2[:,None] - A2[:,None]*C1[None,:])**2) +
+   (A2[:,None] - A1[None,:])*C2[:,None])/((A1[None,:]-A2[:,None])*(C1[None,:] - C2[:,None])+  1e-15)
+  # alpha_2 = (np.sqrt((C1[None,:] - C2[:,None])**2*pess_factor + (A1[None,:]*C2[:,None] - A2[:,None]*C1[None,:])**2) +
+  #  (A2[:,None] - A1[None,:])*C2[:,None])/((A1[None,:]-A2[:,None])*(C1[None,:] - C2[:,None])+ 1e-15)
+  # assert np.isclose(alpha_1[0][1] + alpha_2[1][0], 1)
+  # assert np.isclose(alpha_1[1][0] + alpha_2[0][1], 1)
+  alphas = np.ma.masked_where(~((0<=alpha_1)&(alpha_1<=1)), alpha_1)
+  alphas[1,1] = 0
+  alphas[0,0] = 1
+  # alphas = np.where((0<=alphas)&(alphas<=1), alphas, 0)
+  
+  assert ((0<=alphas)&(alphas<=1)).all()
+  f_alpha = (( A2[:,None] + (A1[None,:] - A2[:,None])*alphas)**2 + pess_factor)/(C2[:,None] + (C1[None,:] - C2[:,None])*alphas + 1e-15)
+  f_0 = (A2[1]**2 + pess_factor)/ (C2[1]+1e-15)
+  f_1 = (A1[0]**2 + pess_factor)/ (C1[0]+1e-15)
+  f_alpha[0,0] = f_1
+  f_alpha[1,1] = f_0
+
+  # f_alpha = np.ma.masked_invalid(f_alpha)
+  assert (f_alpha >= 0).all()
+  # f_alpha[np.eye(num_action, dtype=bool)] = np.inf
+  # f_0[np.eye(num_action, dtype=bool)] = np.inf
+  # f_1[np.eye(num_action, dtype=bool)] = np.inf
+  # f_matrix = np.stack([f_0, f_alpha, f_1], axis=0)
+  index = random_argmax(rng, f_alpha)
+  index = np.unravel_index(index, f_alpha.shape)
+  # assert f_alpha[index[0],index[1]] == f_alpha.min()
+    
+  alpha = alphas[index[0], index[1]]
+  assert not np.isnan(alpha)
+  action = np.zeros(num_action)
+  action[0] = alpha
+  action[1] = 1 - alpha
+  assert ((0<=action)&(action<=1)).all() and action.sum() == 1
+  shortfall = np.zeros([num_action, num_action])
+  shortfall[0,0] = A1[0]**2 + pess_factor
+  shortfall[1,1] = A1[1]**2 + pess_factor
+  shortfall[0,1] = (A1[0]*alpha + A1[1]*(1-alpha))**2 + pess_factor
+  shortfall[1,0] = (A1[0]*(1-alpha) + A1[1]*alpha)**2 + pess_factor
+
+  mutual_info = np.zeros([num_action, num_action])
+  mutual_info[0,0] = C1[0]
+  mutual_info[1,1] = C1[1]
+  mutual_info[0,1] = (C1[0]*alpha + C1[1]*(1-alpha))
+  mutual_info[1,0] = (C1[0]*(1-alpha) + C1[1]*alpha)
+
+
+  return rng.choice(np.arange(num_action), p=action), {'info_ratio': f_alpha, 'shortfall': shortfall, 'mutual_info': mutual_info, 'alpha':alpha, 'Q_hat': Q_hat, 'V_hat': V_hat, 'Q_cross': Q_cross}
+
+def ts_action(rng, pess_factor, t, state, num_trans: np.ndarray, num_success: np.ndarray, num_failure: np.ndarray,
+                   num_good_obs: np.ndarray, num_bad_obs: np.ndarray, num_sample: int = 32):
+  num_action = num_trans.shape[1]
+  num_state = num_trans.shape[0]
+#   num_action = len(num_success)
+
+#   p_samples = rng.beta(np.tile(num_success + 1, (num_sample, 1)), np.tile(num_failure + 1, (num_sample, 1)))
+  trans_shape = num_trans.shape
+  reward_shape = num_success.shape
+  pp = np.zeros((num_state, num_action, num_state))
+  for i in range(num_state-2):
+    if i%2 ==0:
+      pp[i,0,i+2] = 1
+      pp[i,1,i+1] = 1
+    else:
+      pp[i,0,i+2]=1
+      pp[i,1,i+2]=1
+  pp[num_state-2,0,num_state-2]=pp[num_state-1,0,num_state-1]=pp[num_state-2,1,num_state-2]=pp[num_state-1,1,num_state-1]=1
+  p_samples = np.tile(pp, (num_sample, 1,1,1))
+  # p_samples = np.stack([np.stack([rng.dirichlet(num_trans[s,a], size=num_sample) for a in range(num_action)], axis=1) for s in range(num_state)], axis=1)
+  r_samples = rng.beta(np.tile((num_success + 1).reshape(-1), (num_sample, 1)), np.tile((num_failure + 1).reshape(-1), (num_sample, 1)))
+  r_samples = r_samples.reshape(num_sample, *reward_shape)
+  # r_samples[:, np.arange(0, num_state, 2), 0] = 0
+  # r_samples[:, np.arange(1, num_state, 2), :] = 0
+  # r_samples[:,num_state-1,:] = 0
+   
+  c_samples = rng.beta(np.tile((num_good_obs + 1).reshape(-1), (num_sample, 1)), np.tile((num_bad_obs + 1).reshape(-1), (num_sample, 1)))
+  c_samples = c_samples.reshape(num_sample, *reward_shape)
+  c_samples[:, np.arange(0, num_state, 2), 0] = 0
+  c_samples[:, np.arange(1, num_state, 2), :] = 0
+  c_samples[:,num_state-1,:] = 0
+
+  Q_hat, V_hat = value_iter(t, p_samples.reshape(num_sample, *trans_shape), r_samples)
+  Q_cross, _ = value_iter(t, p_samples.reshape(num_sample, *trans_shape), c_samples)
+
+  # Minimize regret
+  A1 = (V_hat[:,state, None] - Q_hat[:,state,:]).mean(0)
+  
+  action = random_argmin(rng, A1**2)
+  assert A1[action] <= A1[1-action]
+
+  return action, {'shortfall': np.stack([A1**2, A1**2], axis=-1), 'Q_hat': Q_hat, 'V_hat': V_hat, 'Q_cross': Q_cross}
 
 class VarIDSAgent(Agent):
-  def __init__(self, compute_action):
+  def __init__(self, compute_action, pess_factor=0):
+    ''' pess_factor controls how pessimistic you are '''
     self._compute_action = compute_action
     self._state = 0
     self._rng = None
-    
-  def reset(self, rng: np.random.Generator, obs_masks: np.ndarray):
-    self._obs_masks = obs_masks
+    self._pess_factor = pess_factor
+  @property
+  def state(self):
+    return self._state
+  def reset(self, rng: np.random.Generator, num_state: int):
     self._num_action = 2
-    self._num_state = obs_masks.shape[0]
+    self._num_state = num_state
     self._rng = rng
     self._state = 0
     
@@ -91,14 +202,18 @@ class VarIDSAgent(Agent):
     self._num_good_obs = np.zeros(shape=(self._num_state, self._num_action,))
     self._num_bad_obs = np.zeros(shape=(self._num_state, self._num_action,))
   
-  def update(self, action: int, next_state, obs: np.ndarray):
-    
+  def update(self, action: int, next_state, obs: np.ndarray, bad_obs:np.ndarray):
     self._num_trans[self.state, action, next_state] += 1
+    reward = obs[self.state, action]
     self._num_success[self.state, action] += obs[self.state, action]
     self._num_failure[self.state, action] += 1 - obs[self.state, action]
     self._num_good_obs += np.nan_to_num(obs)
-    self._num_bad_obs += np.nan_to_num(1 - obs)
+    self._num_bad_obs += np.nan_to_num(bad_obs)
+    self._state = next_state
+    return reward
 
   def select_action(self, t: int):
-    return self._compute_action(self._rng, t, self._state, self._num_trans, self._num_success, self._num_failure,
-                                self.num_good_obs, self.num_bad_obs, self._obs_masks)
+    
+    return self._compute_action(self._rng, self._pess_factor, t, self._state, self._num_trans, self._num_success, self._num_failure,
+                                self._num_good_obs, self._num_bad_obs)
+    
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import pandas as pd
 import plotnine as gg
 import matplotlib.pyplot as plt
 import pylab
+from statistics import mean
 
 from env.graph import Graph
 from agent.var_ids import VarIDSAgent
-from agent.var_ids import var_ids_action
+from agent.var_ids import ts_action, var_ids_pess_action
 
 colors = {0: 'b',
           1: 'g',
@@ -22,84 +23,190 @@ colors = {0: 'b',
 def run_graph_rl_experiment(
     num_sims: int, 
     num_episodes: int, 
+    T: int,
     agent,
     plot_results=False):
     
-  T = 7
-  env = Graph(max_length=T, rewarding_state=3)
+  epsilon = 0.9
+  rewarding_state = 2*T-4
+  env = Graph(max_length=T+1, rewarding_state=rewarding_state)
   
-  num_state = 2*T - 1
-  horizon = T - 1
+  num_state = 2 * T + 1
+  horizon = T + 1
   num_action = 2
   sims = range(num_sims)
   episodes = range(num_episodes)
   num_timesteps = num_episodes*horizon
+  timesteps = range(num_timesteps)
   actions = list(range(num_action))
 
   action_count = []
-  
+  info_ratio_all = []
+  min_episodes = []
+  shortfall_all = []
+  mutual_info_all = []
+
   for sim in sims:
-    rng = np.random.default_rng(sim)
-    
-    agent.reset(rng, obs_masks)
-    action_count += [[num_timesteps*[0] for a in actions]]
-    for episode in episodes:
-      a = agent.select_action() 
-      # Generate observation for all arms.
-      obs = 
-      # Mask the arms that are not observed.
-      mask = 
-      obs[np.logical_not(mask)] = np.nan
-      agent.update(a, obs)
+    rng = np.random.default_rng( )
+    regret = 0
+    agent.reset(rng, num_state)
+    action_count += [[[num_episodes*[np.nan] for a in actions] for s in range(num_state)]]
+    info_ratio_all += [[num_episodes*[np.full((2,2), np.nan)] for s in range(num_state)]]
+    shortfall_all += [[num_episodes*[np.full((2,2), np.nan)] for s in range(num_state)]]
+    mutual_info_all += [[num_episodes*[np.full((2,2), np.nan)] for s in range(num_state)]]
+    for epi in episodes:
+      if epi != 0:
+        assert env.done
+      env.reset()
+      for hh in range(horizon):
+        prev_state = agent.state
+        a, action_stats = agent.select_action(hh) 
+        # Generate observation for all arms.
+        next_state, obs, bad_obs, done = env.step(a)
+        regret -= agent.update(a, next_state, obs, bad_obs)
+        action_count[sim][prev_state][a][epi] = 1
+        info_ratio_all[sim][prev_state][epi] = action_stats['info_ratio']
+        shortfall_all[sim][prev_state][epi] = action_stats['shortfall']
+        mutual_info_all[sim][prev_state][epi] = action_stats['mutual_info']
 
-      action_count[sim][a][timestep] = 1
+        # if epi*horizon+hh % 100==0:
+        #   alpha = action_stats['alpha']
+        #   shortfall = action_stats['shortfall']
+        #   mi = action_stats['mutual_info']
+        #   print(f' State: {prev_state}, Action: {a}, Alpha: {alpha}, \n Shortfall: {shortfall}, \n MI: {mi}')
 
-  action_count1 = [[float(action_count[0][a][t]) 
-    for t in range(num_timesteps)] for a in actions]
-  action_count = [[sum([float(action_count[sim][a][t]) 
-    for sim in sims]) / num_sims 
-    for t in range(num_timesteps)] for a in actions]
+        if prev_state == rewarding_state and  epi % 10==0:
+          alpha = action_stats['alpha']
+          shortfall = action_stats['shortfall']
+          mi = action_stats['mutual_info']
+          Q_hat = action_stats['Q_hat'][:,prev_state,:].mean(0)
+          V_hat = action_stats['V_hat'][:,prev_state].mean(0)
+          Q_cross = action_stats['Q_cross'][:,prev_state,a].mean(0)
+          Q_cross_var = action_stats['Q_cross'][:,prev_state,:].var(0)
+          print(f'Episode: {epi}, State: {prev_state}, Action: {a}, Alpha: {alpha}, \n Shortfall: {shortfall}, \n MI: {mi}, \n Q_hat: {Q_hat}, \n V_hat: {V_hat}, \n Q_cross: {Q_cross}, \n Q_cross variance: {Q_cross_var}')
+          # print(f' State: {prev_state}, Action: {a}, \n Shortfall: {shortfall}, \n Q_hat: {Q_hat}, \n V_hat: {V_hat}, \n Q_cross: {Q_cross}, \n Q_cross variance: {Q_cross_var}')
 
-  # plot action frequencies averaged over simulations
-  for a in actions:
-    plt.plot(timesteps,
-             pd.Series(action_count[a]).rolling(10, min_periods=1).mean(),
-             colors[a], label='$a =$' + str(a+1))
-  plt.axis([0,num_timesteps+1,0.0,1.01])
-  plt.xlabel(r'time $t$', fontsize=20)
-  plt.ylabel('$\mathbb{P}(A_t = a|\mathcal{E})$', fontsize=20)
-#  plt.ylabel('$\mathrm{E}[N_{t,a} - N_{t-1,a}|\mathcal{E}]$', fontsize=20)
-  pylab.legend(loc='best')
-  plt.show()
+
+      regret += 1
+      if ((regret / (epi+1)) < epsilon):
+        min_episodes.append(epi+1)
+        print(f'Sim: {sim}, Average Regret: {(regret / (epi+1))}')
+        break
+
+  action_count1 = [[[(action_count[0][s][a][t]) 
+    for t in range(num_episodes)] for a in actions] for s in range(num_state)]
+  action_count = [[[np.nanmean([(action_count[sim][s][a][t]) 
+    for sim in sims], axis=0)
+    for t in range(num_episodes)] for a in actions] for s in range(num_state)]
+  info_ratio_all = [[np.nanmean([(info_ratio_all[sim][s][t]) 
+    for sim in sims], axis=0)
+    for t in range(num_episodes)] for s in range(num_state)]
+  shortfall_all = [[np.nanmean([(shortfall_all[sim][s][t]) 
+    for sim in sims], axis=0)
+    for t in range(num_episodes)] for s in range(num_state)]
+  mutual_info_all = [[np.nanmean([(mutual_info_all[sim][s][t]) 
+    for sim in sims], axis=0)
+    for t in range(num_episodes)] for s in range(num_state)]
+  # assert len(min_episodes) == num_sims, min_episodes
+  min_episode = mean(min_episodes)
+  print(f'Number of episodes until average regret < {epsilon} = {min_episode}')
+  
+  # # plot action frequencies averaged over simulations
+  # for s in range(num_state):
+  #   for a in actions:
+  #     plt.plot(episodes,
+  #             pd.Series(action_count[s][a]).rolling(10, min_periods=1).mean(),
+  #             colors[a], label='$a =$' + str(a+1))
+  #   plt.axis([0,num_timesteps+1,0.0,1.01])
+  #   plt.xlabel(r'time $t$', fontsize=20)
+  #   plt.ylabel('$\mathbb{P}(A_t = a|\mathcal{E})$', fontsize=20)
+  # #  plt.ylabel('$\mathrm{E}[N_{t,a} - N_{t-1,a}|\mathcal{E}]$', fontsize=20)
+  #   pylab.legend(loc='best')
+  #   plt.show()
 
   # plot action counts over single simulation
-  for a in actions:
-    plt.plot(timesteps, np.cumsum(action_count1[a]), colors[a], label='$a =$' + str(a+1))
+  fig, axes = plt.subplots(horizon, 1, figsize=(10, 5*horizon))
+  for s in range(horizon):
+    for a in actions:
+      axes[s].plot(episodes, np.cumsum(action_count1[2*s][a]), colors[a], label='$a =$' + str(a+1))
+    axes[s].set_title(f'State {2*s} action selection', fontsize=20)
+    axes[s].set_xlabel('Time $t$', fontsize=20)
+    axes[s].set_ylabel('$N_{t,s,a}$', fontsize=20)
   plt.axis([0,num_timesteps+1,0.0,num_timesteps+1])
-  plt.xlabel(r'time $t$', fontsize=20)
-  plt.ylabel('$N_{t,a}$', fontsize=20)
   pylab.legend(loc='best')
-  plt.show()
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('action_count_single')
 
   # plot action counts averaged over simulations
-  for a in actions:
-    plt.plot(timesteps, np.cumsum(action_count[a]), colors[a], label='$a =$' + str(a+1))
+  fig, axes = plt.subplots(horizon, 1, figsize=(10, 5*horizon))
+  for s in range(horizon):
+    for a in actions:
+      axes[s].plot(episodes, np.cumsum(action_count[2*s][a]), colors[a], label='$a =$' + str(a+1))
+    axes[s].set_title(f'State {2*s} action selection', fontsize=20)
+    axes[s].set_xlabel('Time $t$', fontsize=20)
+    axes[s].set_ylabel('$\mathrm{E}[N_{t,s,a}|\mathcal{E}]$', fontsize=20)
   plt.axis([0,num_timesteps+1,0.0,num_timesteps+1])
-  plt.xlabel(r'time $t$', fontsize=20)
-  plt.ylabel('$\mathrm{E}[N_{t,a}|\mathcal{E}]$', fontsize=20)
   pylab.legend(loc='best')
-  plt.show()
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('action_count_expectation')
+
+
+  fig, axes = plt.subplots(horizon, 1, figsize=(10, 5*horizon))
+  for s in range(horizon):
+    for a1 in range(num_action):
+      for a2 in range(num_action):
+        axes[s].plot(episodes, [info_ratio_all[2*s][t][a1,a2] for t in episodes], colors[a1+2*a2], label=f'$a1 =${a1}, $a2 =${a2}')
+        axes[s].set_title(f'State {2*s} Action ({a1},{a2}) info ratio', fontsize=20)
+        axes[s].set_xlabel('Episode $t$', fontsize=20)
+        axes[s].set_ylabel('Expected Info Ratio', fontsize=20)
+  plt.axis([0,num_episodes+1,0.0,num_episodes+1])
+  pylab.legend(loc='best')
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('info_ratio_expectation')
+
+  fig, axes = plt.subplots(horizon, 1, figsize=(10, 5*horizon))
+  for s in range(horizon):
+    for a1 in range(num_action):
+      for a2 in range(num_action):
+        axes[s].plot(episodes, [shortfall_all[2*s][t][a1,a2] for t in episodes], colors[a1+2*a2], label=f'$a1 =${a1}, $a2 =${a2}')
+        axes[s].set_title(f'State {2*s} Action ({a1},{a2}) shortfall', fontsize=20)
+        axes[s].set_xlabel('Episode $t$', fontsize=20)
+        axes[s].set_ylabel('Expected Shortfall', fontsize=20)
+  plt.axis([0,num_episodes+1,0.0,num_episodes+1])
+  pylab.legend(loc='best')
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('shortfall_expectation')
+
+  fig, axes = plt.subplots(horizon, 1, figsize=(10, 5*horizon))
+  for s in range(horizon):
+    for a1 in range(num_action):
+      for a2 in range(num_action):
+        axes[s].plot(episodes, [mutual_info_all[2*s][t][a1,a2] for t in episodes], colors[a1+2*a2], label=f'$a1 =${a1}, $a2 =${a2}')
+        axes[s].set_title(f'State {2*s} Action ({a1},{a2}) mutual info', fontsize=20)
+        axes[s].set_xlabel('Episode $t$', fontsize=20)
+        axes[s].set_ylabel('Expected MI', fontsize=20)
+  plt.axis([0,num_episodes+1,0.0,num_episodes+1])
+  pylab.legend(loc='best')
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('mutual_info_expectation')
 
   return action_count
 
 if __name__ == '__main__':
-    # Variance-IDS agent that learns from reward.
-    var_ids_agent = VarIDSAgent(compute_action=var_ids_action)
-    num_sims = 1000 # number of simulations over which to average
-    num_timesteps = 1000 # number of time steps to simulate
+    # Variance-IDS agent that learns from observation.
+    var_ids_agent = VarIDSAgent(compute_action=var_ids_pess_action, pess_factor=0)
+    num_sims = 100 # number of simulations over which to average
+    num_episodes = 100 # number of time steps to simulate
+    T = 10 # number of possible rewarding_states
 
     action_count_ts_reward = run_graph_rl_experiment(
         num_sims, 
-        num_timesteps, 
+        num_episodes,
+        T,
         agent=var_ids_agent,
         plot_results=True)
